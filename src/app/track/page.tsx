7ea0a34dd82fcc5c4e989b06a9785be2f4feb857
Himanshu Kumar <modesigns.himanshu@gmail.com>
2025-07-16 09:24:46 +0000

    Logo is not showing only its alt or txt showing

--- src/app/track/page.tsx
+++ src/app/track/page.tsx
@@ -1,3 +1,4 @@
+
 import { CustomerTracking } from "@/components/features/tracking/CustomerTracking";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
