abb54eb67d5ab120b2e66f77b80fbc218f5e74f0
Himanshu Kumar <modesigns.himanshu@gmail.com>
2025-07-16 03:46:08 +0000

    It still say invalid cresidencial did really connect to my data base

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,17 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyA1HAseuKX45_j6pUUJhEwGxbtCUW_OnLw",
+  authDomain: "mo-panel.firebaseapp.com",
+  projectId: "mo-panel",
+  storageBucket: "mo-panel.firebasestorage.app",
+  messagingSenderId: "616662408646",
+  appId: "1:616662408646:web:bf730cfb0b9d8de0b326c7",
+  measurementId: "G-M7M6X69SNF"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
