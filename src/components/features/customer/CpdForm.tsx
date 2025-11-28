3268f8a38ced8cbe92c7c043384b74fcf40558ad
Himanshu Kumar <modesigns.himanshu@gmail.com>
2025-11-28 06:44:03 +0000

    Try fixing this error: `Runtime Error: Error: Controller is not defined.

--- src/components/features/customer/CpdForm.tsx
+++ src/components/features/customer/CpdForm.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useForm, useFieldArray, FormProvider, useFormContext, Control, useWatch } from "react-hook-form";
+import { useForm, useFieldArray, FormProvider, useFormContext, Control, useWatch, Controller } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Customer, User, Stock, TaxDetail, Dimension, StitchDimension, AdvanceDetail, CpdItem, CpdRoom } from "@/lib/types";
@@ -419,3 +419,5 @@ function DimensionFields({ roomIndex, itemIndex, dimensionIndex, onRemoveDimensi
     </div>
   )
 }
+
+    
