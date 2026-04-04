4e78eb71c3ed52e5ce0e388abcbbf1e3b551fa42
modesigns-himanshu <modesigns.himanshu@gmail.com>
2026-04-04 18:52:55 +0530

    pms update and fixes by chandan

--- src/app/dashboard/pms/page.tsx
+++ src/app/dashboard/pms/page.tsx
@@ -1,9 +1,5 @@
 import PmsDashboardClient from "./PmsDashboardClient";
 
-<<<<<<< HEAD
-export default function Page() {
-=======
 export default function PmsPage() {
->>>>>>> 3ba0de6 (pms Fixes and update)
   return <PmsDashboardClient />;
 }
